8bcd651beb6fd5a6dc6b1592453fb4ed001295fb
Matt <Matt>
2016-01-19 15:10:35 +0100

    Started working on the parser

--- TwitterTest/src/parser/database/PersonParser.java
+++ TwitterTest/src/parser/database/PersonParser.java
@@ -1,8 +1,8 @@
 package parser.database;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class PersonParser {
@@ -13,18 +13,23 @@ public class PersonParser {
 		this.controller = controller;
 	}
 	
+	/**
+	 * Reads the .csv linked to in path and insets them into the databse via controller
+	 * @param path - path to the csv-File containing the people
+	 */
 	public void parse(final String path){
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(path));
 			StringTokenizer tokens;
-			String line;
+			String line, name;
 			String[] keys = new String[4];
 			//skips header
 			br.readLine();
 			line = br.readLine();
 			while(line != null){
+				//splits the line of the csv into tokens and then add the line to the database
 				tokens = new StringTokenizer(line, ",");
-				String name = tokens.nextToken().trim();
+				name = tokens.nextToken().trim();
 				for(int i = 0; i < keys.length; i++){
 					if(tokens.hasMoreTokens()){
 						keys[i] = tokens.nextToken();
@@ -32,10 +37,9 @@ public class PersonParser {
 				}
 				controller.insertPerson(name, keys);
 				line = br.readLine();
-			}
-			
+			}			
 			br.close();
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
