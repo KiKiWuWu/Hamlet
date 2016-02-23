a8fcd79b6afcb40703d60bc9ae988dbf9916e0ab
Matt <Matt>
2016-02-23 15:22:26 +0100

    Made parser work by simply traversing the XML-Tree recursively.

--- TwitterTest/src/parser/xmlParsing/ElementList.java
+++ TwitterTest/src/parser/xmlParsing/ElementList.java
@@ -2,6 +2,7 @@ package parser.xmlParsing;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -9,155 +10,97 @@ import org.jdom2.Element;
 import org.jdom2.Namespace;
 
 /**
- * This class is designed to hold a list of {@link Element}, iterate over it and extract the data relevant for the database.
- * For getting the correct sequence of lines across different instances of this class over the same data, 
- * the method {@code getCurrentLineNumber} should be used.
+ * This class is designed to hold a list of {@link Element}, iterate over it and
+ * extract the data relevant for the database. For getting the correct sequence
+ * of lines across different instances of this class over the same data, the
+ * method {@code getCurrentLineNumber} should be used.
+ * 
  * @author rom54494
  *
  */
 public class ElementList {
-	
-	private final String TAG_SPEECH = "ab";
-	private final String TAG_SEGMENT = "seg";
+
+	private final String TAG_SPEECH = "sp";
+	private final String TAG_STAGE = "stage";
+	private final String TAG_HEAD = "head";
 	private final String TAG_SPEAKER = "speaker";
 	private final String TAG_WORD = "w";
 	private final String TAG_CHAR = "c";
 	private final String TAG_PUNC = "pc";
 	private final String TAG_LB = "lb";
-	private final String ATTR_ID = "id";
 	private final String ATTR_WHO = "who";
 
-	private Namespace ns;
-	private Iterator<Element> iter;
-	private Element currentElement;
-	private int currentLine;
-	private String currentText;
-	private String currentSpeaker;
-	
-	/**
-	 * @param list A list of {@link Element}s of a certain kind containing all lines to be parsed
-	 * @param ns Namespace for the Elements, may be {@code null} for no namespace
-	 */
-	public ElementList(List<Element> list, Namespace ns){
-		this.ns = ns;
-		currentLine = Integer.MAX_VALUE;
-		iter = list.iterator();
-		setNext();
+	private LinkedList<Tweet> currentTweets;
+	private Element head;
+	private int ct = 0;
+
+	public ElementList(Element head) {
+		this.head = head;
+		currentTweets = new LinkedList<Tweet>();
 	}
-	
+
 	/**
-	 * Updates {@code currentLine}. For this, {@code currentElement} is examined and parsed
-	 * accordingly to whether it is a stage-direction or something said by an actor 
+	 * After going to the next line, the speaker has to be updated if possible
+	 * (not for stage-directions etc.)<br>
+	 * Will set {@code currentSpeaker} to {@code null} if no speaker can be
+	 * found.
 	 */
-	private void updateText(){
-		List<Element> textList;
+	private String updateSpeaker(Element currentElement) {
+		String tag = currentElement.getName();
+		if (tag.equals(TAG_SPEECH)) {
+			return currentElement.getAttributeValue(ATTR_WHO);
+		} 
+		else if(tag.equals(TAG_STAGE)){
+			return "Regie";
+		}
+		else{
+			return "";
+		}
+	}
+
+	private void addTweet(String speaker, String line) {
+		if (line.equals("") == false) {
+			currentTweets.add(new Tweet(speaker, line, null, ++ct));
+		}
+	}
+
+	private void parseTree(Element head, String speaker) {
 		String line = "";
-		if(currentElement.getChild(TAG_SPEECH, ns) != null){
-			if (currentElement.getChild(TAG_SPEECH, ns).getChild(TAG_SEGMENT, ns) != null){//appears in songs
-				textList = currentElement.getChild(TAG_SPEECH, ns).getChild(TAG_SEGMENT, ns).getChildren();
-			}
-			else{//speaker text
-				textList = currentElement.getChild(TAG_SPEECH, ns).getChildren();
+		String tmp = updateSpeaker(head);
+		if(tmp != ""){
+			speaker = tmp;
+		}
+		LinkedList<Element> children = new LinkedList<Element>(head.getChildren());
+		if (children.size() == 0) {
+			return;
+		}
+		for (Element child : children) {
+			String tag = child.getName();
+			switch (tag) {
+			case TAG_HEAD:
+			case TAG_SPEAKER:
+				break;
+			case TAG_CHAR:
+			case TAG_WORD:
+			case TAG_PUNC:
+				line += child.getText();
+				break;
+			case TAG_LB:
+				addTweet(speaker, line);
+				line = "";
+				break;
+			default:
+				addTweet(speaker, line);
+				line = "";
+				parseTree(child, speaker);
+				break;
 			}
 		}
-		else{//stage-direction
-			textList = currentElement.getChildren();
-		}
-		for(Element e : textList){
-			String tag = e.getName();
-			if(tag.equals(TAG_WORD) || tag.equals(TAG_CHAR)|| tag.equals(TAG_PUNC)){//character
-				line += e.getText();
-			}
-			else if(tag.equals(TAG_LB)){//linebreak
-				line += System.getProperty("line.separator");
-			}
-		}
-		currentText = line;
+		addTweet(speaker, line);
 	}
-	
-	/**
-	 * Takes a look at {@code currentElement} and reads the number of the current line from it.
-	 * @return the new line number
-	 */
-	private void updateLineNumber(){
-		String line;
-		if(currentElement.getChild(TAG_SPEECH, ns) != null){//speaker-text
-			if(currentElement.getChild(TAG_SPEECH, ns).getChild(TAG_SEGMENT, ns) != null){
-				line = currentElement.getChild(TAG_SPEECH, ns).getChild(TAG_SEGMENT, ns).getChild(TAG_WORD, ns)
-						.getAttribute(ATTR_ID, Namespace.XML_NAMESPACE).getValue();
-			}
-			else{ //songs etc.
-				line = currentElement.getChild(TAG_SPEECH, ns).getChild(TAG_WORD, ns)
-						.getAttribute(ATTR_ID, Namespace.XML_NAMESPACE).getValue();
-			}
-		}
-		else{//stage-direction
-			line = currentElement.getChild(TAG_WORD, ns).getAttribute(ATTR_ID, Namespace.XML_NAMESPACE).getValue();
-		}
-		line = line.replace("w", "");
-		currentLine = Integer.parseInt(line);
-	}
-	
-	/**
-	 * After going to the next line, the speaker has to be updated if possible (not for stage-directions etc.)<br>
-	 * Will set {@code currentSpeaker} to {@code null} if no speaker can be found.
-	 */
-	private void updateSpeaker(){
-		if(currentElement.getChild(TAG_SPEAKER, ns) != null){
-			currentSpeaker = currentElement.getAttributeValue(ATTR_WHO);
-		}
-		else{
-			currentSpeaker = null;
-		}
-	}
-	
-	/**
-	 * Looks at {@code currentElement} and parses the drama-text from it.
-	 * @return All strings for the current spoken text
-	 */
-	public List<String> getCurrentLineText(){
-		ArrayList<String > result = new ArrayList<>();
-		StringTokenizer tokenizer = new StringTokenizer(currentText, System.getProperty("line.separator"));
-		if(tokenizer.hasMoreTokens() == false){//only one line
-			result.add(currentText);
-		}
-		else{
-			while(tokenizer.hasMoreTokens()){
-				result.add(tokenizer.nextToken());
-			}
-		}
-		return result;
-	}
-	
-	/**
-	 * @return the number of the line in the current element. Returns Integer.MAX_VALUE if no line is specified (head-tag!!)
-	 */
-	public int getCurrentLineNumber(){
-		return currentLine;
-	}
-	
-	/**
-	 * @return The person who is saying the current line, {@code null} if no speaker
-	 */
-	public String getCurrentSpeaker(){
-		return currentSpeaker;
-	}
-	
-	/**
-	 * Tells the class to update the current element
-	 * @return true, if the element has been updated successfully
-	 * <br> false, if there were no more elements in the list and no update was possible
-	 */
-	public boolean setNext(){
-		if(iter.hasNext()){
-			currentElement = iter.next();
-			updateLineNumber();
-			updateText();			
-			updateSpeaker();
-			return true;
-		}
-		else{
-			return false;
-		}
+
+	public List<Tweet> getTweets() {
+		parseTree(head, null);
+		return currentTweets;
 	}
 }
--- TwitterTest/src/parser/xmlParsing/XMLParser.java
+++ TwitterTest/src/parser/xmlParsing/XMLParser.java
@@ -20,8 +20,6 @@ import parser.xmlParsing.ElementList;
  */
 public class XMLParser {
 	
-	/** This array contains the tags of the XML which contain relevant text. */
-	private final String[] TEXT_TAGS = {"stage", "sp"};
 	/** Namespace for the xml, is read from it directly */
 	private Namespace ns;
 	/** Responsible for handling the insertion of tweets into the database */
@@ -35,7 +33,6 @@ public class XMLParser {
 	/** These hold the XML for the acts or scenes respectively */
 	private ArrayList<Element> acts, scenes;
 	/** These hold the specific elements to be processed for each scene */
-	private ArrayList<ElementList> lists;
 	
 	/**
 	 * @param controller Receives the lines when the are parsed
@@ -54,39 +51,6 @@ public class XMLParser {
 		acts = new ArrayList<Element>(body.getChildren("div1", ns));
 	}
 	
-	private LinkedList<Element> buildResultList(Element head, String tag){
-		LinkedList<Element> result = new LinkedList<Element>();
-		if(head.getName().equals(tag)){
-			result.add(head);
-		}
-		else{
-			for(Element e : head.getChildren()){
-				if(e.getName().equals(tag)){
-					result.add(e);
-				}
-				else{
-					result.addAll(buildResultList(e, tag));
-				}
-			}
-		}
-		return result;
-	}
-	
-	/**
-	 * Creates the array {@code lists} with the same size as {@code TEXT_TAGS} and creates for each
-	 * tag in this array an {@link ElementList} with the data in {@code body}.
-	 */
-	private void initLists(Element scene){
-		lists = new ArrayList<ElementList>();
-		for(int i = 0; i < TEXT_TAGS.length; i++){
-			LinkedList<Element> resultList = new LinkedList<Element>();
-			for(Element e : scene.getChildren()){
-				resultList.addAll(buildResultList(e, TEXT_TAGS[i]));
-			}
-			lists.add( new ElementList(resultList, ns) );
-		}
-	}
-	
 	/**
 	 * Fills {@code scenes} for the current act
 	 * @param act The current act from which the scenes shall be extraced
@@ -96,45 +60,12 @@ public class XMLParser {
 	}
 	
 	/**
-	 * Checks if at least one {@link ElementList} in {@code lists} has elements left.
-	 * @return true if there are elements left to parse
-	 * <br> false if all elements have been parsed and added to the database
-	 */
-	private boolean elementsLeft(){
-		if(lists.size() == 0){
-			return false;
-		}
-		else{
-			return true;
-		}
-	}
-	
-	/**
-	 * Looks at all {@link ElementList}s of {@code list} and determines which has the next line
-	 * to be processed.
-	 * @return The index of the ElementList in {@code lists} which holds the next line to be processed.
-	 */
-	private int getNextLine(){
-		int index = -1;
-		int min = Integer.MAX_VALUE;
-		for(int i = 0; i < lists.size(); i++){
-			int line = lists.get(i).getCurrentLineNumber();
-			if(line < min){
-				min = line;
-				index = i;
-			}
-		}
-		return index;
-	}
-	
-	/**
 	 * Gives the controller a {@link Tweet} with the initial informations about the scene to follow
 	 * @param act the current act
 	 * @param scene the current scene
 	 */
-	private void constructInitialSceneTweet(int act, int scene){
-		Tweet t = new Tweet(null, "Act " + act + " Scene " + scene, "tweet");
-		controller.insertLine(t);
+	private Tweet constructInitialSceneTweet(int act, int scene){
+		return new Tweet(null, "Act " + act + " Scene " + scene, "tweet", -1);
 	}
 	
 	/**
@@ -143,29 +74,16 @@ public class XMLParser {
 	 * {@code insertLine} of the {@link DBCInterface}.
 	 */
 	public void run(){
-		Tweet t;
-		int curr;
-		String speaker;
-		ArrayList<String> textList;
-		/*ArrayList<Tweet> tl = new ArrayList<Tweet>();
-		tl.sort(new TweetComperator());*/
+		ArrayList<Tweet> tl = new ArrayList<Tweet>();
+
 		for(int numAct = 3; numAct < 4; numAct++){
 			initScenes(acts.get(numAct));
 			for(int numScene = 4; numScene < 5; numScene++){
-				constructInitialSceneTweet(numAct+1, numScene+1);
-				initLists(scenes.get(numScene));
-				while(elementsLeft()){
-					curr = getNextLine();
-					speaker = lists.get(curr).getCurrentSpeaker();
-					textList = new ArrayList<String>(lists.get(curr).getCurrentLineText());
-					for(String s : textList){
-						t = new Tweet(speaker, s, null);
-						controller.insertLine(t);
-					}
-					//removes list so it doesn't have to be checked every time if elements are left
-					if(lists.get(curr).setNext() == false){
-						lists.remove(curr);
-					}
+				tl.add(constructInitialSceneTweet(numAct+1, numScene+1));;
+				ElementList elist = new ElementList(scenes.get(numScene)); 
+				tl.addAll(elist.getTweets());
+				for(Tweet t : tl){
+					controller.insertLine(t);
 				}
 			}				
 		}
