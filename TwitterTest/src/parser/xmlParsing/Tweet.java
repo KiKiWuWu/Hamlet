87d2229c3fd82c032e5213c6f75a66ed99b39780
Matt <Matt>
2016-02-23 15:56:04 +0100

    Removing now unnecessary features from Tweet.java and adding comments
    Changed the way the acts/scenes are handled to work for any number of scenes

--- TwitterTest/src/parser/xmlParsing/Tweet.java
+++ TwitterTest/src/parser/xmlParsing/Tweet.java
@@ -12,22 +12,15 @@ public class Tweet {
 	private String text;
 	/** Will it be handled as a Tweet, Response or something else */
 	private String type;
-	/** Number of the line/character */
-	private int lineNumber;
 	
 	/**
 	 * 
 	 * @param speaker The person who said the line
 	 * @param text The line itself
-	 * @param type Type how it will be handled for Twitter (Tweet, Response etc.)
+	 * @param type Type how it will be handled for Twitter (Tweet, Response etc.), may be null for default
 	 */
-	public Tweet(String speaker, String text, String type, int lineNumber){
-		if(speaker == null){ 
-			this.speaker = "Regie";
-		}
-		else{
-			this.speaker = speaker;
-		}
+	public Tweet(String speaker, String text, String type){
+		this.speaker = speaker;
 		
 		if(type == null){
 			this.type = "response";
@@ -40,8 +33,6 @@ public class Tweet {
 		}
 		
 		this.text = text;
-		
-		this.lineNumber = lineNumber;
 	}
 	
 	/**
@@ -65,31 +56,12 @@ public class Tweet {
 	public String getType(){
 		return type;
 	}
-	
-	public int getLineNumber(){
-		return lineNumber;
-	}
-	
+
 	/**
 	 * @return A String consisting of "speaker : text"
 	 */
 	@Override
 	public String toString(){
-		//return speaker + ": " + text + " , " + lineNumber;
 		return speaker + ": " + text;
 	}
-	/**
-	 * Adds a string which determines if the line is to be handled as a response, a retweet or a normal tweet
-	 * @param type String, may only be of ... 
-	 * @return true if the type has been changed successfully <br> false if the type couldn't be changed	 * 
-	 */
-	/*public void setType(String type){
-		if(type.equals("tweet") == false){
-			this.type = "response";
-		}
-		else{
-			this.type = type;
-		}
-	}*/
-	
 }
